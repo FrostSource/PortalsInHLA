4c1dc1ec8004af4e6f02a86981426514eef58651
FrostSource <jjhfrost@gmail.com>
2025-04-10 00:22:25 +1200

    Update AlyxLib

--- scripts/vscripts/alyxlib/player/core.lua
+++ scripts/vscripts/alyxlib/player/core.lua
@@ -960,9 +960,12 @@ function CBasePlayer:UpdateWeaponsExistence()
     if #weapons.genericpistols > 0 then
         for i = #weapons.genericpistols, 1, -1 do
             local generic = weapons.genericpistols[i]
-            local swt = Entities:FindByName(nil, "wpnswitch_" .. generic:GetName())
-            if not swt then
-                table.remove(weapons.genericpistols, i)
+            -- Server change seems to destroy weapons before this is run
+            if IsValidEntity(generic) then
+                local swt = Entities:FindByName(nil, "wpnswitch_" .. generic:GetName())
+                if not swt then
+                    table.remove(weapons.genericpistols, i)
+                end
             end
         end
     end
