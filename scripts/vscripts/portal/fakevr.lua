9380a2c0c1e9508818cd7fb41a2426bae7ffbdef
FrostSource <jjhfrost@gmail.com>
2025-09-06 13:35:18 +1200

    fakevr

---
+++ scripts/vscripts/portal/fakevr.lua
@@ -0,0 +1,16 @@
+Convars:RegisterCommand("portal_fire_portalgun_blue", function ()
+    if PortalManager.portalGun then
+        PortalManager.portalGun:TryFirePortal(PortalManager.colors.orange)
+    end
+end, "", 0)
+Convars:RegisterCommand("portal_fire_portalgun_orange", function ()
+    if PortalManager.portalGun then
+        PortalManager.portalGun:TryFirePortal(PortalManager.colors.orange)
+    end
+end, "", 0)
+
+ListenToPlayerEvent("vr_player_ready", function(params)
+    print("Player is in fakevr mode...")
+    SendToConsole("bind 1 portal_fire_portalgun_blue")
+    SendToConsole("bind 2 portal_fire_portalgun_orange")
+end)
